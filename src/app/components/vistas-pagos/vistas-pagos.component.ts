3002e6f3a3605abaa6f4f995a2182146649b27d2
CrespoD <davidcrespo941@gmail.com>
2022-03-06 23:48:52 -0600

    6

--- src/app/components/vistas-pagos/vistas-pagos.component.ts
+++ src/app/components/vistas-pagos/vistas-pagos.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from  '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper' ;
 
 @Component({
   selector: 'app-vistas-pagos',
   templateUrl: './vistas-pagos.component.html',
   styleUrls: ['./vistas-pagos.component.css']
 })
-export class VistasPagosComponent implements OnInit {
 
+export class VistasPagosComponent implements OnInit {
+  /**
   constructor() { }
 
   ngOnInit(): void {
-  }
+  }*/
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = false;
 
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 }
